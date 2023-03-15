c2c5a5cf751df47649036c5bd90e544c8b3cd7e0
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-15 20:04:58 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -176,13 +176,13 @@ def main_function(airport1, airport2):
                 files = os.listdir(os.getcwd())
                 for i in files:
                     try:
-                        os.remove(os.getcwd() + i)
+                        os.remove(os.getcwd() + r'/{}'.format(i))
                     except Exception:
                         continue
                 st.write(os.listdir(os.getcwd()))
                 for i in range(5):
                     scraping_function(main_url+flight_links[i]+"/tracklog", elevation1, elevation2)
-                st.write(os.listdir(os.getcwd() + r'\Datasets'))
+                st.write(os.listdir(os.getcwd() + r'/Datasets'))
                 return "CSV's have been created"
                 break
             else:
