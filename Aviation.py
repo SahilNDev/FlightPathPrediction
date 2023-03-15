2b593fb2d54065427dc6ebb1769e7e0a13ad6b76
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-15 20:20:03 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -174,6 +174,12 @@ def main_function(airport1, airport2):
             elevation2 = airports[airports['gps_code'] == airport2].reset_index(drop=True)['elevation_ft'][0]*0.3048
             if len(flight_links) == 5:
                 path = os.path.join(os.getcwd(), r'Datasets')
+                files = os.listdir(path)
+                for i in files:
+                    try:
+                        os.remove(path + r'\{}'.format(i))
+                    except Exception:
+                        continue
                 st.write(os.listdir(path))
                 for i in range(5):
                     scraping_function(main_url+flight_links[i]+"/tracklog", elevation1, elevation2)
