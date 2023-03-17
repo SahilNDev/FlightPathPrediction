7cbf4b31abbbb22894db5e0561ca7b68dd64874b
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-17 22:23:47 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -215,8 +215,8 @@ def main_function(airport1, airport2):
                 break
             else:
                 set1.remove(flight)
-    except Exception:
-        return "No flights are there between {} and {}, please change the locations and try again.".format(airports[airports['gps_code']==airport1].reset_index()['municipality'][0], airports[airports['gps_code']==airport2].reset_index()['municipality'][0])
+    except Exception as e:
+        return e
     
 df = pd.read_csv("in-airports.csv")
 def add_bg_from_url():
