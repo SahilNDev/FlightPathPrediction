dfc07a590b54677784c206d6c35bed48e3e50cae
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-20 11:21:17 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -43,6 +43,7 @@ def create_dataset(dataset, look_back, look_ahead):
 def model_implementation():
     csvs = glob('/Datasets/*.csv')
     csv_list = [csv[:-5] for csv in csvs]
+    st.write(csv_list)
     csv_list.sort()
     datelist = []
     for i in csv_list:
