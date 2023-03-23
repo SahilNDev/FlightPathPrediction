1c07141d1e935426164ed732d8316cb4602a541c
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-23 15:23:31 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -280,7 +280,7 @@ def scraping_function(url, s_elevation, e_elevation):
         lat2, lon2, alt2 = get_point_at_distance(df['Latitude'][n-1], df['Longitude'][n-1],  df['meters'][n-1], d/1000, df['Course'][n-1], 0)
         df.loc[n] = ['', lat2, lon2, df['Course'][n-1], df['kts'][n-1], (df['m/s'][n-1]-deceleration)*9/4, alt2, df['Rate'][n-1], '', 1, df['m/s'][n-1]-deceleration, d, 0]
     fig = px.line_3d(df, x="Longitude", y = "Latitude", z="meters", title = "{}-{}-{}".format(url[-27:-25], url[-29:-27], url[-33:-29]))
-    st.plotly_chart(fig, use_container_width = True, theme = None)
+    st.plotly_chart(fig, use_container_width = True, theme = "streamlit")
     
     df.to_csv(r"Datasets/{}-{}-{}.csv".format(url[-27:-25], url[-29:-27], url[-33:-29]), index = False)
     return "{}-{}-{}".format(url[-27:-25], url[-29:-27], url[-33:-29])
