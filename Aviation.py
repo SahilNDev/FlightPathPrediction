8622451715831e344eb0fb9a2880bdcdb22ef7b2
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-23 19:11:53 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -269,8 +269,7 @@ def scraping_function(url, s_elevation, e_elevation, flight, s, e):
         lat2, lon2, alt2 = get_point_at_distance(df['Latitude'][n-1], df['Longitude'][n-1],  df['meters'][n-1], d/1000, df['Course'][n-1], 0)
         df.loc[n] = ['', lat2, lon2, df['Course'][n-1], df['kts'][n-1], (df['m/s'][n-1]-deceleration)*9/4, alt2, df['Rate'][n-1], '', 1, df['m/s'][n-1]-deceleration, d, 0]
     fig = px.line_3d(df, x="Longitude", y = "Latitude", z="meters", title = "Trajectory of the plane {} on {}-{}-{}".format(flight, url[-27:-25], url[-29:-27], url[-33:-29]))
-    fig.add_annotation(x=df['Longitude'][0], y=df['Latitude'][0], z=df['meters'][0],text=s)
-    fig.add_annotation(x=df['Longitude'][len(df)-1], y = df['Latitude'][len(df)-1], z=df['meters'][len(df)-1],text=e)
+    fig.text(x=df['Longitude'][0], y=df['Latitude'][0], z=df['meters'][0],text=s)
     st.plotly_chart(fig, use_container_width = True)
     
     df.to_csv(r"Datasets/{}-{}-{}.csv".format(url[-27:-25], url[-29:-27], url[-33:-29]), index = False)
