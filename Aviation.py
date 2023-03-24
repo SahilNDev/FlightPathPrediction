09e06fb5fc7d388161f12f1bde5b9484014c10b9
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-24 15:40:44 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -344,6 +344,7 @@ def main_function(airport1, airport2):
                     fileslist.append(file)
                     convertingToKML(file, s, e)
                 st.write("CSV's and KML's have been created")
+                fileslist.sort()
                 st.write(fileslist)
                 model_implementation()
                 return
