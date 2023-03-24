5b3aa34e5b7b4b32ebed97a0146199d9448ebecd
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-24 09:47:10 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -27,7 +27,7 @@ from keras.callbacks import EarlyStopping
 import base64
 
 def get_download_link(df,x):
-    csv = df.to_csv(x, index = False)
+    csv = df.to_csv(index = False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
     href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
 
