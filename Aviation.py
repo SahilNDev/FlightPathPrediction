7d91c46bf8c7c09e20d51718cf377528510be3c2
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-24 16:05:59 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -107,7 +107,7 @@ def model_implementation(files):
         Y_train = scaler.inverse_transform([Y_train])
         test_predict = scaler.inverse_transform(test_predict)
         Y_test = scaler.inverse_transform([Y_test])
-        units_dict[i] = test_predict
+        units_dict[i] = test_predict[:,0]
         st.write(f'Train Mean Absolute Error for {i}:', mean_absolute_error(Y_train[0], train_predict[:,0]))
         st.write(f'Train Root Mean Squared Error for {i}:',np.sqrt(mean_squared_error(Y_train[0], train_predict[:,0])))
         st.write(f'Test Mean Absolute Error for {i}:', mean_absolute_error(Y_test[0], test_predict[:,0]))
