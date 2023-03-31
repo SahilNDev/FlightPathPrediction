e70f5b861949ef12ef01f2b05764225b7cccda7b
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-31 15:20:53 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -299,12 +299,13 @@ def main_function(airport1, airport2):
                 for i in range(5):
                     file = scraping_function(main_url+flight_links[i]+"/tracklog", elevation1, elevation2, flight,s,e)
                     fileslist.insert(0, file)
-                return fileslist, flight,s,e, og
+                st.write(og)
+                return fileslist,flight,s,e,og
             else:
                 flights.remove(flight)
     except Exception as ex:
         st.write(f"No flights are there between {s} and {e}, change the locations and try again.")
-        return "", "", "", ""
+        return "", "", "", "", ""
     
 df = pd.read_csv("in-airports.csv")
 def add_bg_from_url():
