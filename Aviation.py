b5f7368b1b1ed0b4ca6da4825d43e2b9d2ceb62e
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-03-31 12:14:00 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -367,7 +367,7 @@ if tk == 1:
                     for r,rows in df.iterrows():
                         loc.append((rows['Latitude'], rows['Longitude']))
                     folium.PolyLine(loc, color = 'red', weight=5, opacity = 0.8).add_to(m)
-                    folium_static(m)
+                    st_folium(m)
                 l1, l2 = convertingToKML(i, s, e, flight)
                 st.markdown(l1, unsafe_allow_html = True)
                 st.markdown(l2, unsafe_allow_html = True)
