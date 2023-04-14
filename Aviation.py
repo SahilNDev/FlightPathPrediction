cd96b8ed06cdbfbd4ff1b6c5bb342c8dde5a8814
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2023-04-14 17:34:57 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -77,7 +77,7 @@ def model_implementation(files, flight, og, look_back, look_ahead):
     og_p = []
     for i in units:
         arr = []
-        pr = dataframelist[-1][i][:(look_back+look_ahead)]
+        pr = dataframelist[-1][i][:(look_back+look_ahead-1)]
         df_update = dataframelist[0].loc[:,['date_time',i, 'day', 'hour','minute','second', 'Course', 'tilt']]
         for df in dataframelist[1:]:
             df_lat=df.loc[:,['date_time',i, 'day', 'hour','minute','second', 'Course', 'tilt']]
@@ -136,16 +136,17 @@ def model_implementation(files, flight, og, look_back, look_ahead):
         plt.legend(fontsize=10)
         arr.append(fig1)
         units_dict[i] = arr
-#         if og != None:
-#             og_lat=og_df.loc[:,['date_time',i, 'day', 'hour','minute','second', 'Course', 'tilt']]
-#             og_dataset =og_lat[i].values #numpy.ndarray
-#             og_dataset = og_dataset.astype('float32')
-#             og_dataset = np.reshape(og_dataset, (-1, 1))
-#             og_test = scaler.transform(og_dataset)
-#             og_test = np.reshape(og_test, (og_test.shape[0], 1, og_test.shape[1]))
-#             og_predict = model.predict(og_test)
-#             og_predict = scaler.inverse_transform(og_predict)
-#             og_p.append("The next {} is: {}".format(i, og_predict[-1,0]))
+        if og != None:
+             og_lat=og_df.loc[:,['date_time',i, 'day', 'hour','minute','second', 'Course', 'tilt']]
+             og_dataset =og_lat[i].values #numpy.ndarray
+             og_dataset = og_dataset.astype('float32')
+             og_dataset = np.reshape(og_dataset, (-1, 1))
+             og_dataset = scaler.transform(og_dataset)
+	     og_test = np.array(og_dataset[-lookback:, 0])
+             og_test = np.reshape(og_test, (og_test.shape[0], 1, og_test.shape[1]))
+             og_predict = model.predict(og_test)
+             og_predict = scaler.inverse_transform(og_predict)
+             og_p.append("The next {} is: {}".format(i, og_predict[0,0]))
     predicted_df.to_csv(r"Datasets/{}-{}.csv".format(flight, 'Predicted'), index = False)
     return units_dict, og_p
 	
@@ -360,10 +361,10 @@ st.title("Predict Flight Path Between Two Locations :airplane:")
 col1, col2 = st.columns(2)
 with col1:
     origin = st.selectbox('Origin: ', set(df['Display Name']), index = 0)
-    look_behind = st.slider('Look Behind',5,20, help = "It is the past n data points used for making the next prediction.")
+    look_behind = st.slider('Look Behind',5,20, help = "It is the past n data points being used for making the next prediction.")
 with col2:
     destination = st.selectbox('Destination: ', tuple(df[df['Display Name']!=origin]['Display Name']))
-    look_ahead = st.slider("Look Ahead",1,10, help = "It is the nth future point being predicted.")
+    look_ahead = st.slider("Look Ahead",1,10, help = "It is the nth future point being predicted, in intervals of 30 seconds.")
     if st.button('Submit'):
         tk = 1
 if tk == 1:
