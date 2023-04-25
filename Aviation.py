a069231ca50ed2b9e2e166a52cee932ef557b36e
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-04-25 11:12:40 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -339,11 +339,13 @@ def destination_maker(origin):
     flights = pd.DataFrame(columns = ['iata_code', 'Display Name', 'Flight'])
     for i in trs:
         tds = i.find_all('td')
-        st.write(tds)
-        tds[2] = re.findall(r"\([A-Z]+\)", tds[2].text)[0][1:-1]
-        st.write(tds[2])
-        if tds[2] in airports['iata_code']:
-            flights.loc[flights.shape[0]] = [tds[2],airports[airports['iata_code'] == tds[2]].reset_index(drop=True)['Display Name'][0] ,tds[0].text.replace(" ","")]
+        try:
+            tds[2] = re.findall(r"\([A-Z]+\)", tds[2].text)[0][1:-1]
+            st.write(tds[2])
+            if tds[2] in airports['iata_code']:
+                flights.loc[flights.shape[0]] = [tds[2],airports[airports['iata_code'] == tds[2]].reset_index(drop=True)['Display Name'][0] ,tds[0].text.replace(" ","")]
+	except Exception:
+            continue
     return flights
 df = pd.read_csv("in-airports.csv")
 def add_bg_from_url():
