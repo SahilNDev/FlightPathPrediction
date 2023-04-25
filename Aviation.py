11dd355bfc439c9be474bf6f22f49b8a1e102488
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2023-04-25 11:09:55 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -339,8 +339,7 @@ def destination_maker(origin):
     flights = pd.DataFrame(columns = ['iata_code', 'Display Name', 'Flight'])
     for i in trs:
         tds = i.find_all('td')
-        st.write(type(tds))
-        tds[2] = re.findall(r"\([A-Z]+\)", tds[2].text)
+        tds[2] = re.findall(r"\([A-Z]+\)", tds[2].text)[0][1:-1]
         st.write(tds[2])
         if tds[2] in airports['iata_code']:
             flights.loc[flights.shape[0]] = [tds[2],airports[airports['iata_code'] == tds[2]].reset_index(drop=True)['Display Name'][0] ,tds[0].text.replace(" ","")]
