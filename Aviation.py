af4c35a8d2b4ffa9d0673c07f2192061d1d8f5bd
Sahil Nagaralu <92047320+SahilNDev@users.noreply.github.com>
2025-05-27 19:54:37 +0530

    Update Aviation.py

--- Aviation.py
+++ Aviation.py
@@ -318,7 +318,7 @@ def main_function(airport1, airport2, flight):
             num = 0
             while num < 5:
                 st.write(main_url+flight_links[i]+"/tracklog")
-		try:
+                try:
                     file = scraping_function(main_url+flight_links[i]+"/tracklog", elevation1, elevation2, flight,s,e)
                     fileslist.insert(0, file)
 		    num += 1
